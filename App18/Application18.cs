1617ce0bd038367568113e09b29687ae4eb26afa
Mihai Gonciar <mihaigonciar26@gmail.com>
2025-03-16 02:11:19 +0200

    PR

---
+++ App18/Application18.cs
@@ -0,0 +1,22 @@
+﻿using ITSchool.AppUtilities;
+
+namespace App18
+{
+    internal class Application18 : ApplicationBase
+    {
+        public Application18()
+        {
+            Cerinta = "Realizati o aplicatie de tip consola in care sa indepliniti urmatoarele cerinte, in ordine:" +
+                "Creati o clasa numita “Button” in care sa definiti un eveniment “Clicked”. Evenimentul va fi declansat printr-o metoda publica numita “SimulateClick”. Elementele trebuie sa fie parte din obiecte.\n" +
+                "Creati o clasa numita “Window”. Aceasta se va abona la evenimentul “Clicked” al oricarui buton adaugat in Constructor, iar listenerul va afisa un mesaj in consola.\n" +
+                "Extindeti clasa Button, adaugand ca argument la eveniment numele butonului. Modificati clasa Window astfel incat denumirea butonului apasat sa fie transmisia.\n" +
+                "Implementati un mecanism de citire de la tastatura in bucla. La introducerea corecta a denumirii unui buton existent, acestuia ii va fi apelata metoda “SimulateClick”.\n" +
+                "Limitati elementele de cod astfel incat doar acele proprietati / metode dorite sa fie expuse.";
+        }
+
+        public override void Launch()
+        {
+
+        }
+    }
+}
---
+++ App18/Button.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection.Metadata;
+
+namespace App18
+{
+    public class Button
+    {
+        public event Action<string> Clicked;
+
+        public string Name { get; set; }
+
+        public Button(string name)
+        {
+            Name = name;
+        }
+
+        public void SimulateClick()
+        {
+            Clicked?.Invoke(Name);
+        }
+    }
+
+    public class Window
+    {
+        public Window()
+        {
+            // Am putea pastra o lista cu toate butoanele create aici
+
+            Button button = new Button("Buton 1");
+            button.Clicked += CandUnButonEsteApasat;
+
+            Button button2 = new Button("Buton 2");
+            button2.Clicked += CandUnButonEsteApasat;
+
+            Button button3 = new Button("Buton 3");
+            button3.Clicked += CandUnButonEsteApasat;
+
+            Button button4 = new Button("Buton 4");
+            button4.Clicked += CandUnButonEsteApasat;
+
+            Button button5 = new Button("Buton 5");
+            button5.Clicked += CandUnButonEsteApasat;
+        }
+
+        public void CandUnButonEsteApasat(string name)
+        {
+            Console.WriteLine($"Butonul {name} a fost apasat.");
+        }
+    }
+}
